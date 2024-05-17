7fb9043975adf708891ab2068a18474dde02999e
Ryan White <ryanwhite04@gmail.com>
2024-05-17 12:55:07 +0800

    Add arw_mrna to python path

--- mrnaid/backend/flask_app/app.py
+++ mrnaid/backend/flask_app/app.py
@@ -3,6 +3,7 @@ import os
 
 # Add the directory containing Evaluation.py to the PYTHONPATH
 sys.path.append(os.path.abspath("../common"))
+sys.path.append(os.path.abspath("../common/arw_mrna/src"))
 print(sys.executable)
 from .routes import app
 
