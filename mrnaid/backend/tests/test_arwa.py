8d473434104a36370563ef6dd4a8525f4d6f6d30
Ryan White <ryanwhite04@gmail.com>
2024-10-16 22:11:01 +0800

    Test arwa algorithm
    cds is hardcoded as passing seed to random_cds doesn't seem to work yet for some reason, if you know why please let me know
    
    ```sh
    python -m unittest tests/test_arwa
    ```

--- mrnaid/backend/tests/test_arwa.py
+++ mrnaid/backend/tests/test_arwa.py
@@ -1,20 +1,35 @@
 from unittest import main, TestCase
 from common.arw_mrna.src.awalk import adaptive_random_walk, WalkConfig
-from common.arw_mrna.src.protein import CodonFrequencyTable
+from common.arw_mrna.src.protein import CodonFrequencyTable, random_cds
 from common.arw_mrna.src.objective_functions import make_cai_threshold_obj, make_cai_and_efe_obj, make_cai_and_aup_obj, CAIThresholdObjectiveConfig
 
 class TestARWA(TestCase):
-    
+        
     def test_adaptive_random_walk(self):
-        walk_config = WalkConfig(
-            aa_seq="MVSKGEELFTGVVPILVELDGDVNGH",
-            freq_table=CodonFrequencyTable("homosapiens.txt"),
-            objective=make_cai_and_efe_obj,
-            steps=100,
-            verbose=True,
-            init_cds=None
+        aa_seq = "MVSKGEELFTGVVPIL"
+        freq_table = CodonFrequencyTable("homosapiens.txt")
+        objective = make_cai_and_efe_obj(
+            CAIThresholdObjectiveConfig(
+                freq_table=freq_table,
+                cai_threshold=0.8,
+                cai_exp_scale=1.0,
+                verbose=False
+            )
         )
-        result = adaptive_random_walk(walk_config)
+        cds = ['AUG', 'GUU', 'UCC', 'AAG', 'GGA', 'GAA', 'GAA', 'CUC', 'UUC', 'ACU', 'GGU', 'GUU', 'GUU', 'CCG', 'AUC', 'UUG', 'GUU', 'GAA', 'CUC', 'GAU', 'GGA', 'GAU', 'GUA', 'AAU', 'GGA', 'CAC']
+        
+        walk_config = WalkConfig(
+            aa_seq,
+            freq_table,
+            objective,
+            steps=1,
+            init_cds=cds,
+            seed=42
+        )
+        generator = adaptive_random_walk(walk_config)
+        initial = next(generator)
+        self.assertEqual(initial.step, 0)
+        self.assertAlmostEqual(initial.best_fitness, 16.45672606, places=6)
     
 if __name__ == '__main__':
     main()
