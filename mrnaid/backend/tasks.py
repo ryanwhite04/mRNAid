78024dc1c422704c91cb381710bfac3b7478bcde
Ryan White <ryanwhite04@gmail.com>
2024-10-17 19:46:44 +0800

    Use localhost as default PRIVATE_HOST
    for running locally not in docker without a .env file

--- mrnaid/backend/tasks.py
+++ mrnaid/backend/tasks.py
@@ -19,12 +19,13 @@ from datetime import datetime, timedelta
 logger = MyLogger(__name__)
 
 NUMBER_OF_ATTEMPTS = 3
-PRIVATE_HOST = os.getenv('PRIVATE_HOST', "flask")
+PRIVATE_HOST = os.getenv('PRIVATE_HOST', "localhost")
 PRIVATE_PORT = os.getenv('PRIVATE_PORT', 5000)
 PRIVATE_URL = f"http://{PRIVATE_HOST}:{PRIVATE_PORT}"
 PUBLIC_HOST = os.getenv('PUBLIC_HOST', "localhost")
 PUBLIC_PORT = os.getenv('PUBLIC_PORT', 5000)
 PUBLIC_URL = f"http://{PUBLIC_HOST}:{PUBLIC_PORT}"
+logger.info(f"{PUBLIC_URL=}")
 celery = Celery('tasks')
 celery.config_from_object('celery_config')
 sio = Client()
@@ -35,6 +36,7 @@ def init_pool():
 @task_prerun.connect
 def task_prerun_handler(sender=None, **kwargs):
     if not sio.connected:
+        logger.info(f"{PUBLIC_URL=}")
         sio.connect(PRIVATE_URL)
 
 @task_postrun.connect
